3e92189dfbefe9bd1a19122dc8e649cfc487a2fc
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-08-15 15:35:50 +0800

    Update FB.js

--- Scripts/FB.js
+++ Scripts/FB.js
@@ -7,5 +7,40 @@
 hostname= api.revenuecat.com
 
 *******************************/
+{
+	"request_date_ms": 416505600000,
+	"request_date": "1983-03-15T00:00:00Z",
+	"subscriber": {
+		"non_subscriptions": {
+			"filebox_pro": [{
+				"id": "yingzi",
+				"is_sandbox": false,
+				"purchase_date": "1983-03-15T00:00:00Z",
+				"original_purchase_date": "1983-03-15T00:00:00Z",
+				"store": "app_store"
+			}]
+		},
+		"first_seen": "1983-03-15T00:00:00Z",
+		"original_application_version": "162",
+		"other_purchases": {
+			"filebox_pro": {
+				"purchase_date": "1983-03-15T00:00:00Z"
+			}
+		},
+		"management_url": null,
+		"subscriptions": {
 
-{"request_date_ms":416505600000,"request_date":"1983-03-15T00:00:00Z","subscriber":{"non_subscriptions":{"filebox_pro":[{"id":"yingzi","is_sandbox":false,"purchase_date":"1983-03-15T00:00:00Z","original_purchase_date":"1983-03-15T00:00:00Z","store":"app_store"}]},"first_seen":"1983-03-15T00:00:00Z","original_application_version":"162","other_purchases":{"filebox_pro":{"purchase_date":"1983-03-15T00:00:00Z"}},"management_url":null,"subscriptions":{},"entitlements":{"filebox_pro":{"grace_period_expires_date":null,"purchase_date":"1983-03-15T00:00:00Z","product_identifier":"filebox_pro","expires_date":"2999-03-15T00:00:00Z"}},"original_purchase_date":"1983-03-15T00:00:00Z","original_app_user_id":"$RCAnonymousID:88af1c116b274f27aaec084a31ebe80f","last_seen":"1983-03-15T00:00:00Z"}}
+		},
+		"entitlements": {
+			"filebox_pro": {
+				"grace_period_expires_date": null,
+				"purchase_date": "1983-03-15T00:00:00Z",
+				"product_identifier": "filebox_pro",
+				"expires_date": "2999-03-15T00:00:00Z"
+			}
+		},
+		"original_purchase_date": "1983-03-15T00:00:00Z",
+		"original_app_user_id": "$RCAnonymousID:88af1c116b274f27aaec084a31ebe80f",
+		"last_seen": "1983-03-15T00:00:00Z"
+	}
+}
