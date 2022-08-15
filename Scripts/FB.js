5b37f212fc8b74ba99d2d8bb9e9f32bdc627f530
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2022-08-15 15:05:36 +0800

    Update FB.js

--- Scripts/FB.js
+++ Scripts/FB.js
@@ -1,15 +1,9 @@
-
-
 [rewrite_local]
 # Fileball 解锁高级版
 # 下载地址：http://mtw.so/5X5nGK
 ^https?:\/\/api\.revenuecat\.com\/v\d\/(subscribers/\$RCAnonymousID\%(\w)+|receipts)$ url echo-response text/Json echo-response https://raw.githubusercontent.com/chxm1023/QX/main/Scripts/FB.js
-
 [mitm]
 hostname= api.revenuecat.com
-
-
-
 {
   "request_date_ms" : 416505600000,
   "request_date" : "1983-03-15T00:00:00Z",
